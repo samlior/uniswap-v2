be8acbfd5a85c9539be10330d4303c75cc8f7e52
samlior <samlior@foxmail.com>
2023-05-12 16:53:59 +0800

    feat: add WETH

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,13 +3,26 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.6.6",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
+    compilers: [
+      {
+        version: "0.6.6",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
       },
-    },
+      {
+        version: "0.4.18",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
   },
 };
 
